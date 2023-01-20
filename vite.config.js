97d855c44c2bdb5dc2115ffc4f7dbfbab9735504
Elliot <elliot.cln@gmail.com>
2023-01-20 18:28:17 +0100

    Init boilerplate /w Wordplate

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+
+require('dotenv').config();
+
+export default defineConfig(() => ({
+  publicDir: 'resources/static',
+  build: {
+    assetsDir: '',
+    emptyOutDir: true,
+    manifest: true,
+    outDir: `public/themes/${process.env.WP_DEFAULT_THEME}/assets`,
+    rollupOptions: {
+      input: 'resources/js/index.js',
+    },
+  },
+  plugins: [
+    {
+      name: 'php',
+      handleHotUpdate({ file, server }) {
+        if (file.endsWith('.php')) {
+          server.ws.send({ type: 'full-reload', path: '*' });
+        }
+      },
+    },
+  ],
+}));
